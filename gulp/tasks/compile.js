c75cb0e3c11749a5138001d57cc771296426e056
Fabian van Steen <mail@fabianvansteen.nl>
2015-06-19 13:44:24 +0200

    Removed not used javascript files.

--- gulp/tasks/compile.js
+++ gulp/tasks/compile.js
@@ -1,5 +1,14 @@
+'use scrict';
+
 var gulp = require('gulp');
+var path = require('path');
 
 gulp.task('compile', function () {
   gulp.start(['jshint', 'less']);
+
+  var angular = path.resolve('node_modules', 'angular', 'angular.js');
+  var jsDest = path.join('src', 'client', 'assets', 'js');
+
+  gulp.src([angular])
+    .pipe(gulp.dest(jsDest));
 });
--- src/server/routes/static.js
+++ src/server/routes/static.js
@@ -1,11 +1,14 @@
 'use scrict';
 
+var express = require('express');
+
 exports.addRoutes = function (app, config) {
-  
-  // Serve up the favicon
-  app.use(config.server.staticUrl + '/site.css', function (req, res, next) {
-    res.sendfile('site.css', { root: config.server.distFolder + '/less' });
-  });
+    
+  // JavaScript
+  app.use(config.server.staticUrl, express.static(config.server.distFolder + '/assets/'));
+
+  // JavaScript
+  app.use('/app', express.static(config.server.distFolder + '/app/'));
 
   app.use(config.server.staticUrl, function (req, res, next) {
     res.send(404); // If we get here then the request for a static file is invalid
