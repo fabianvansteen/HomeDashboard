8a4ca7037f8a3b2e9f58717294fd58c2f711f0d3
Fabian van Steen <mail@fabianvansteen.nl>
2015-07-31 23:03:58 +0200

    - Add express the proxy for getting version information through an api. - Added a config file for the proxy configuration/express port and proxy port.

--- proxy/app.js
+++ proxy/app.js
@@ -1,37 +1,55 @@
-var http = require('http');
-var httpProxy = require('http-proxy');
+/* global write */
+'use strict'
 
-var port = 8080;
+/**
+ * Module dependencies.
+ */
+var http = require('http'),
+	httpProxy = require('http-proxy'),
+	express = require('express'),
+	VersionHandler = require('./handlers/versionHandler'),
+	config = require('./config.json');
 
-var proxy = httpProxy.createProxyServer({});
+var proxyPort = config.proxy.port,
+	expressPort = config.express.port,
+	proxy = httpProxy.createProxyServer({}),
+	app = express();
 
-var addresses = {
-	'dashboard.home.dev:8080': {
-		host: 'localhost',
-		port: 20000
-	},
-	'weather.home.dev:8080': {
-		host: 'localhost',
-		port: 20010
-	},
-	'temperature.home.dev:8080': {
-		host: 'localhost',
-		port: 20011
-	},
-	'calendar.home.dev:8080': {
-		host: 'localhost',
-		port: 20012
+/**
+  * HTTP GET /api/version
+  * Returns: Return the application version information.
+  */
+app.get('/version', function (request, response) {
+	var handler = new VersionHandler();
+
+	var version = handler.retrieveVersion();
+
+	response.status(200).json(version);
+});
+
+var expressServer = app.listen(expressPort, function () {
+
+	var host = expressServer.address().address;
+	var port = expressServer.address().port;
+
+	console.log('listening at http://%s:%s', host, port);
+});
+
+var proxyServer = http.createServer(function (req, res) {
+
+	var target = config.routes[req.headers.host.split(':')[0]];
+	
+	if (!target) {
+		res.writeHead(404, { "Content-Type": "text/plain" });
+		res.write("404 Not Found\n");
+		res.end();
+		return;
 	}
-};
-
-var server = http.createServer(function (req, res) {
-
-	var target = (addresses[req.headers.host]) ? addresses[req.headers.host] : addresses.default;
 		
 	// You can define here your custom logic to handle the request
 	// and then proxy the request.
 	proxy.web(req, res, { target: 'http://' + target.host + ':' + target.port });
 });
 
-console.log("listening on port " + port)
-server.listen(port);
+console.log("listening on port " + proxyPort)
+proxyServer.listen(proxyPort);
